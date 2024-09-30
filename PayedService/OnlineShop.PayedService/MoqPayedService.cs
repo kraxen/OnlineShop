acac8e625cee78fe7526dfed5379aebc6179540d
Виталий Яровенко <yarovenko_va@ascon.ru>
2024-10-01 02:38:05 +0400

    Добавил заглушку сервиса оплаты

---
+++ PayedService/OnlineShop.PayedService/MoqPayedService.cs
@@ -0,0 +1,17 @@
+﻿using OnlineShop.ServiceInterfaces;
+
+namespace OnlineShop.PayedService
+{
+    public class MoqPayedService : IPayedService
+    {
+        public bool Pay(decimal cost)
+        {
+            return true;
+        }
+
+        public async Task<bool> PayAsync(decimal cost)
+        {
+            return Pay(cost);
+        }
+    }
+}
